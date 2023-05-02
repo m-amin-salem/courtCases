a2382258f4a36b3f41d41f1a997446dada118436
Mohammed Amin Salem <m.amin.salem@hotmail.com>
2023-05-03 01:54:11 +0300

    Initial code base

---
+++ README.md
@@ -0,0 +1,12 @@
+## Court cases
+API for create and get cases
+
+### Endpoints
+1. http://localhost:3000/api/case
+2. http://localhost:3000/api/allcases
+
+### How to use
+1. cd to courtCases directory
+2. run `$ npm i`
+3. run `$ npm start`
+4. Use any API platform (such Postman) to consume the endpoints
---
+++ index.ts
@@ -0,0 +1,30 @@
+import express, { NextFunction, Request, Response } from 'express';
+import { CaseDto } from './src/case.dto.';
+import { getCases, setCounter, generateFxFileId, addNewCase } from './src/helpers'
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/api/case', (req: Request, res: Response) => {
+  const cases = getCases()
+  const counter = setCounter(cases)
+  const currentYear = new Date().getFullYear().toString().substr(-2)
+  const fxFileId = generateFxFileId(req.body.customerName, currentYear, counter);
+  const newCase: CaseDto = { ...req.body, fxFileId };
+  
+  addNewCase(newCase, cases)
+
+  res.send(newCase);
+});
+
+app.get('/api/allcases', (req: Request, res: Response) => {
+  const cases = getCases()
+
+  res.send(cases);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
